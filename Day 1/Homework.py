8938e9a0005a16c2a75c84e778dfcf76c2ac9455
JakeyoloxD <148301003+JakeyoloxD@users.noreply.github.com>
2023-10-28 18:30:14 +1100

    6:30pm

--- Day 1/Homework.py
+++ Day 1/Homework.py
@@ -9,3 +9,4 @@ print("String Concatenation is done with the "+" sign.")
 print('e.g. print("Hello" + "world")')
 print("New lines can be created with a backslash and n.")
 
+print("Hello" + " " + "World!")
