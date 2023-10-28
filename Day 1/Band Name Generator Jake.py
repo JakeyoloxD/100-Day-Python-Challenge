f254bc3fae6284cc87ffd831e99325fa25b34d9d
JakeyoloxD <148301003+JakeyoloxD@users.noreply.github.com>
2023-10-28 16:51:47 +1100

    Debug complete

--- Day 1/Band Name Generator Jake.py
+++ Day 1/Band Name Generator Jake.py
@@ -4,7 +4,9 @@ print("print('what to print')")
 
 # Fix the code below 👇
 
+# Fix the code below 👇
+
 print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print(" Hello " + " world ")')
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello" + "world")')
 print("New lines can be created with a backslash and n.")
