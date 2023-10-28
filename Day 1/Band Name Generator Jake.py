89603c90a5a0196fa0df6f281d6647487ada4a30
JakeyoloxD <148301003+JakeyoloxD@users.noreply.github.com>
2023-10-28 18:44:09 +1100

    band name complete

--- Day 1/Band Name Generator Jake.py
+++ Day 1/Band Name Generator Jake.py
@@ -1,12 +1,11 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+#1. Create a greeting for your program.
+print("Welcome to the band name generator")
+#2. Ask the user for the city that they grew up in.
+colour = input("What is your favourite colour?\n ")
+#3. Ask the user for the name of a pet.
+animal = input("What is your favourite animal?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + colour + " " + animal + ".")
+#5. Make sure the input cursor shows on a new line:
 
-# Fix the code below 👇
-
-# Fix the code below 👇
-
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-print('e.g. print("Hello" + "world")')
-print("New lines can be created with a backslash and n.")
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
---
+++ Day 1/homework 2.py
@@ -0,0 +1,12 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Fix the code below 👇
+
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
